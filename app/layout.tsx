8577888aa3903901fc164d83c5be3adc26464da1
zined <zinedinarnaut085@gmail.com>
2024-01-21 14:20:55 +1100

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,10 @@ import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "@/components/navbar";
 import "./globals.css";
-
+import Provider from "@/components/Provider";
 import Footer from "@/components/footer"
 
-import GridPattern from "@/components/grid-pattern";
+import GridPattern from "@/components/magicui/grid-pattern";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -31,6 +31,15 @@ export default function RootLayout({
       </ThemeProvider>
 <Toaster />
       <Footer />
+      <GridPattern
+          width={40}
+          height={40}
+          x={-1}
+          y={-1}
+          className={
+              "-z-10 stroke-gray-300/30 [mask-image:linear-gradient(to_bottom_left,white,transparent,transparent)] "
+          }
+      />
       </body>
       </html>
   );
--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -88,7 +88,7 @@ const PricingCard = ({ isYearly, title, monthlyPrice, yearlyPrice, description, 
 
 const CheckItem = ({ text }: { text: string }) => (
     <div className="flex gap-2">
-        <CheckCircle2 size={18} className="my-auto text-green-400" />
+        <CheckCircle2 size={18} className="my-auto text-primary" />
         <p className="pt-0.5 text-zinc-700 dark:text-zinc-300 text-sm">{text}</p>
     </div>
 )
